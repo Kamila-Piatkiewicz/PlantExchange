f06507eb09a969470db00c3601b08c226a728498
Kamila Piątkiewicz <k.piatkiewicz46@gmail.com>
2020-04-05 08:22:05 +0200

    Initialize Spring Boot application

---
+++ src/main/java/com/plantexchange/plantexchange/PlantexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.plantexchange.plantexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantexchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantexchangeApplication.class, args);
+    }
+
+}
